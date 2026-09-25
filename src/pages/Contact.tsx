d0ac5cad6a52dfe7ef0027763b2e592f9eb4683c
Joshkean01 <adekunlej960@gmail.com>
2026-09-25 20:19:59 +0100

    style:align left col on contact page to the center y-axis

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div className="px-6 py-14 md:px-12 lg:px-16 xl:py-14 2xl:py-24 min-h-full flex flex-col justify-center">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-10 xl:gap-12 2xl:gap-16 items-start max-w-7xl mx-auto w-full">
-        <div className="lg:col-span-5 space-y-7 2xl:space-y-8">
+        <div className="lg:col-span-5 space-y-7 2xl:space-y-8 h-full flex flex-col justify-center">
           <div className="space-y-3 2xl:space-y-4">
             {/* tighter for 1366-ish */}
             <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-3xl 2xl:text-5xl font-bold text-gray-400">
